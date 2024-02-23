a45f5f6be77913a5fbf38e9ce15f9010bdd09861
Jun Hwan <junwchoi85@gmail.com>
2024-02-23 20:36:09 +1300

    Adding UserPlacesProvider

---
+++ lib/providers/user_places.dart
@@ -0,0 +1,17 @@
+import 'package:favorite_places_app/models/place.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+/// The State managed by riverpod must not be mutated.
+/// You must not edit it in memory.
+/// Instead, if you want to add somthing to it,
+/// you should create a new state object.
+class UserPlacesNotifier extends StateNotifier<List<Place>> {
+  UserPlacesNotifier() : super(const []); // pass initial state to super.
+
+  void addPlace(String title) {
+    final newPlace = Place(title: title);
+    state = [newPlace, ...state];
+  }
+}
+
+final userPlacesProvider = StateNotifierProvider((ref) => UserPlacesNotifier());
