0136a77ff9eb12aa22ecd2f45f473f73fd4f5b8e
Jun Hwan <junwchoi85@gmail.com>
2024-02-29 11:20:54 +1300

    Storing data to local DB

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path_provider/path_provider.dart' as syspaths;
 import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
 
 import 'package:favorite_places_app/models/place.dart';
 
@@ -13,11 +15,31 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []); // pass initial state to super.
 
   void addPlace(String title, File image, PlaceLocation location) async {
+    /// Copy the image to the app's directory.
     final appDir = await syspaths.getApplicationDocumentsDirectory();
     final fileName = path.basename(image.path);
     final copiedImage = await image.copy('${appDir.path}/$fileName');
 
-    final newPlace = Place(title: title, image: copiedImage, location: location);
+    final newPlace =
+        Place(title: title, image: copiedImage, location: location);
+
+    /// access the local DB
+    final dbPath = await sql.getDatabasesPath(); // get the path to the DB.
+    final db = await sql.openDatabase(path.join(dbPath, 'places.db'),
+        onCreate: (db, version) {
+      return db.execute(
+          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, loc_lat REAL, loc_lng REAL, address TEXT)');
+    }, version: 1);
+
+    db.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+      'loc_lat': newPlace.location.latitude,
+      'loc_lng': newPlace.location.longitude,
+      'address': newPlace.location.address,
+    });
+
     state = [newPlace, ...state];
   }
 }
